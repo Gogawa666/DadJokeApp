92a51fab04237c4e6a91f29bb7065110d8063f0f
Gogawa666 <gogawa@lcs.on.ca>
2021-02-19 11:23:32 -0500

    Initial Commit

---
+++ DadJoke/DadJokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokeApp.swift
+//  DadJoke
+//
+//  Created by Gaku Ogawa on 2021/02/19.
+//
+
+import SwiftUI
+
+@main
+struct DadJokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
